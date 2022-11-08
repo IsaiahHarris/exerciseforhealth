55e0e1b4f339e6c32678ce7a700b1198968d2f29
IsaiahHarris <isaiah.harris.hi@gmail.com>
2022-11-08 09:47:03 -1000

    load configuration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.scss";
+import Button from "./Button/Button.js";
+import React, { useState } from "react";
+import { loadDataAPI } from "./mockbackend";
 function App() {
+  const [coinsurance, setCoinsurance] = useState();
+  const [PPOPlan, setPPOPlan] = useState();
+  const [configurationEnabled, setConfigurationEnabled] = useState();
+  const loadData = () => {
+    const canLoadConfiguration = loadDataAPI();
+    console.log(canLoadConfiguration);
+    setConfigurationEnabled(canLoadConfiguration.loadScreen);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="header-container">
+        <Button text="Load" click={loadData} />
+        <Button text="Save" />
+        <select
+          type="radio"
+          value={coinsurance}
+          defaultValue
+          onChange={(e) => setCoinsurance(e.target.value)}
         >
-          Learn React
-        </a>
-      </header>
+          <option disabled value>
+            - Coinsurance Plan -
+          </option>
+          <option value="hmsa">HMSA</option>
+          <option value="uhc">UHC</option>
+        </select>
+
+        <select
+          type="radio"
+          value={PPOPlan}
+          defaultValue
+          onChange={(e) => setPPOPlan(e.target.value)}
+        >
+          <option disabled value>
+            - PPO Plan -
+          </option>
+          <option value="hmsa">HMSA</option>
+          <option value="uhc">UHC</option>
+        </select>
+      </div>
+      {configurationEnabled && (
+        <div className="configuration-container">this is configuration</div>
+      )}
     </div>
   );
 }
---
+++ src/mockbackend.js
@@ -0,0 +1,5 @@
+export const loadDataAPI = () => {
+  return {
+    loadScreen: true,
+  };
+};
